6ca8c7eaf8ee07b529aa39cb32b2672306ff06db
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-11-29 14:25:46 +1030

    Initial commit

---
+++ abs_download.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+'''
+loop over each sale page, saving the content of the web page to disk
+
+https://www.codementor.io/@aviaryan/downloading-files-from-urls-in-python-77q3bs0un
+'''
+
+script_folder = os.path.dirname(__file__)
+at_values = [{
+    'url':'https://www.audible.com/ep/black-friday-week-sale-2021-sff?pageSize=50&page=',
+    'page_length':4,
+    'filename':'sff'
+    },{
+    'url':'https://www.audible.com/ep/black-friday-week-sale-2021-mt?pageSize=50&page=',
+    'page_length':6,
+    'filename':'mt'
+}]
+
+for x in at_values:         # loop over each category in at values
+    # print(x['url'])
+    for y in range(1,x['page_length']+1):   # load each page url, limit is manually set in varible page length
+        url = x['url']+str(y)               # add the page number to the url
+        r = requests.get(url, allow_redirects=True) # download the page
+        filename = script_folder + '\\downloads\\' + x['filename'] + str(y) + '.html'   # build the filename
+        open(filename,'wb').write(r.content)    # save the web page         
+
---
+++ abs_reader.py
@@ -0,0 +1,44 @@
+import os
+from bs4 import BeautifulSoup
+
+html_folder = os.path.dirname(__file__) + '\\downloads\\'
+
+''' file loader  '''
+test_file = html_folder + 'mt1.html'
+
+
+def read_html(filename,debug=0):
+    '''audible html file -> book listing as dictionary items in a list'''
+    soup = BeautifulSoup(open(filename, encoding='utf-8'), 'lxml')      # load the file
+    books = soup.findAll('li',class_='productListItem')         # find all the book items on the page
+    book_list = []
+
+    for book in books:
+        dict = {}
+        dict['name'] = book.find('h2').get_text()
+        
+        author = book.find(class_='authorLabel')        # find the first item that matches that class
+        author = list(author.children)[1].text          # select the second child        
+        author = author.replace('By:','').strip()       # strip out characters
+        dict['author'] = author
+        
+        stars = book.find(string=lambda text: "stars" in text.lower())          # find the field that contains ""
+        dict['stars'] = stars.replace('out of 5 stars','')                      # strip chars
+        
+        rating = book.find(string=lambda text: "ratings" in text.lower())
+        rating = int(str(rating).split()[0].replace(',',''))            
+        dict['ratings'] = rating
+        
+        url = book.find('a',class_='bc-link')
+        url = url['href'].split('/')[-1]
+        dict['url'] = url
+        
+        dict['full url'] = 'www.audible.com/pd/'+ url
+
+        book_list.append(dict)
+    if debug:
+        print(len(book_list),'items processed')
+    if debug > 1:
+        print('first book is',book_list[0])
+    return book_list
+
